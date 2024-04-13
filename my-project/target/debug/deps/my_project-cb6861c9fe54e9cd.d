14c002dc39dde00e4053df12566d3ecb7db727fa
uestc-zb <2459262592@qq.com>
2024-04-13 17:15:35 +0800

    ch07 finish

---
+++ my-project/target/debug/deps/my_project-cb6861c9fe54e9cd.d
@@ -0,0 +1,5 @@
+/home/star/project/rustLearning/my-project/target/debug/deps/libmy_project-cb6861c9fe54e9cd.rmeta: src/main.rs
+
+/home/star/project/rustLearning/my-project/target/debug/deps/my_project-cb6861c9fe54e9cd.d: src/main.rs
+
+src/main.rs:
