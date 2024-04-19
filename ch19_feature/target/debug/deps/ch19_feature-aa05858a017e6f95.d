04d6f2011daa4f75bd9b9844c5344ad239115970
uestc-zb <2459262592@qq.com>
2024-04-19 09:33:06 +0800

    ch18 finish

---
+++ ch19_feature/target/debug/deps/ch19_feature-aa05858a017e6f95.d
@@ -0,0 +1,5 @@
+/home/star/project/rustLearning/ch19_feature/target/debug/deps/libch19_feature-aa05858a017e6f95.rmeta: src/main.rs
+
+/home/star/project/rustLearning/ch19_feature/target/debug/deps/ch19_feature-aa05858a017e6f95.d: src/main.rs
+
+src/main.rs:
